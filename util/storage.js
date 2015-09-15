1b3cd8c5148c1706f127dfdd28f2965e561c06ef
Dan Calinescu <hi@dancali.io>
2015-09-15 05:37:47 +0300

    fixed querying

--- util/storage.js
+++ util/storage.js
@@ -13,6 +13,10 @@ var storage = {
     var db = storage.db();
 
     db.serialize(function() {
+      // Create the table if necessary
+      var fields = storage.fields(controller);
+      db.run("CREATE TABLE IF NOT EXISTS " + controller.name + " (id INTEGER PRIMARY KEY,timestamp TEXT," + fields.fields + ")");
+
       db.all("SELECT * FROM " + controller.name, function(err, rows) {
         if (err) {
           callback(err);
@@ -26,45 +30,55 @@ var storage = {
     db.close();
   },
 
+  fields: function(controller, request) {
+    // Compound the fields
+    var result = {
+      fields     : [],
+      fieldNames : [],
+      values     : []
+    }
+
+    for(field in controller.schema) {
+      var type  = controller.schema[field].type;
+      var value;
+
+      if (request) {
+        value = request.body[field];
+      }
+
+      if (type === 'EMAIL' || type === 'DATE' || type === 'PHONE') {
+        type = 'TEXT';
+      }
+
+      if (type === 'TEXT') {
+        value = "'" + value + "'";
+      }
+
+      result.fieldNames.push(field);
+      result.fields.push(field + " " + type);
+      result.values.push(value);
+    }
+
+    result.fields     = result.fields.join(",");
+    result.fieldNames = result.fieldNames.join(",");
+    result.values     = result.values.join(",");
+
+    return result;
+  },
+
   save: function(controller, request) {
 
     // Create the db file if necessary
     var db = storage.db();
 
     db.serialize(function() {
-
-      // Compound the fields
-      var fields     = [];
-      var fieldNames = [];
-      var values     = [];
-
-      for(field in controller.schema) {
-        var type  = controller.schema[field].type;
-        var value = request.body[field];
-
-        if (type === 'EMAIL' || type === 'DATE' || type === 'PHONE') {
-          type = 'TEXT';
-        }
-
-        if (type === 'TEXT') {
-          value = "'" + value + "'";
-        }
-
-        fieldNames.push(field);
-        fields.push(field + " " + type);
-        values.push(value);
-      }
-
-      fields     = fields.join(",");
-      fieldNames = fieldNames.join(",");
-      values     = values.join(",");
-
       // Create the table if necessary
-      var now = new Date().toISOString().slice(0,19).replace(/T/g," ");
-      db.run("CREATE TABLE IF NOT EXISTS " + controller.name + " (id INTEGER PRIMARY KEY,timestamp TEXT," + fields + ")");
+      var fields = storage.fields(controller, request);
+      db.run("CREATE TABLE IF NOT EXISTS " + controller.name + " (id INTEGER PRIMARY KEY,timestamp TEXT," + fields.fields + ")");
 
       // Insert a row
-      var stmt = db.prepare("INSERT INTO " + controller.name + " (timestamp," + fieldNames + ") VALUES ('" + now + "'," + values + ")");
+      var now = new Date().toISOString().slice(0,19).replace(/T/g," ");
+      var stmt = db.prepare("INSERT INTO " + controller.name + " (timestamp," + fields.fieldNames + ") VALUES ('" + now + "'," + fields.values + ")");
       stmt.run();
       stmt.finalize();
     });
